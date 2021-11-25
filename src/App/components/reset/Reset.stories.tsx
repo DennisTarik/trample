dd2d2a35541b6e567d74f7d4641dfa1f38900994
DennisTarik <dm499555@gmail.com>
2021-11-25 13:04:51 +0100

    :sparkles: add reset history and logout button for settings

---
+++ src/App/components/reset/Reset.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Reset from './Reset';
+
+export default {
+  component: Reset,
+  title: 'Components/reset',
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
+
+export const Default = () => <Reset />;
