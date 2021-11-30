f09a4affcdf34ad60f88d520ba0f618e6f35838b
DennisTarik <dm499555@gmail.com>
2021-11-30 09:41:28 +0100

    :sparkles: add roadbutton

---
+++ src/App/components/Taskbar/Roadbutton/Roadbutton.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Roadbutton from './Roadbutton';
+
+export default {
+  component: Roadbutton,
+  title: 'Components/Roadbutton',
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
+
+export const Default = () => <Roadbutton />;
