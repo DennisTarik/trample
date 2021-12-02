afb004b2841f4606b9fea525a1118f47370dc026
DennisTarik <dm499555@gmail.com>
2021-12-02 12:30:04 +0100

    :zap: add dynamic fill of settingsbutton

--- src/App/components/Taskbar/Taskbar.tsx
+++ src/App/components/Taskbar/Taskbar.tsx
@@ -3,7 +3,7 @@ import Roadbutton from './Roadbutton/Roadbutton';
 import Settingsbutton from './Settingsbutton/Settingsbutton';
 import styles from './Taskbar.module.css';
 
-export default function Tasbar(): JSX.Element {
+export default function Taskbar(): JSX.Element {
   return (
     <div className={styles.taskbar}>
       <Homebutton />
