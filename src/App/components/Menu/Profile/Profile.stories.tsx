298838aec02da0dd47b539606e4b976d25f0ae76
DennisTarik <dm499555@gmail.com>
2021-12-06 21:14:54 +0100

    :construction: working on components for menu page

---
+++ src/App/components/Menu/Profile/Profile.stories.tsx
@@ -0,0 +1,11 @@
+import Profile from './Profile';
+
+export default {
+  component: Profile,
+  title: 'Components/Profile',
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
+
+export const Default = () => <Profile />;
