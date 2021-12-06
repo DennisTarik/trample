6ec260e24010296f36f218df227b41680308bda6
DennisTarik <dm499555@gmail.com>
2021-12-06 15:52:33 +0100

    :sparkles: add Signup component

---
+++ src/App/components/Signup/Signup.stories.tsx
@@ -0,0 +1,8 @@
+import Signup from './Signup';
+
+export default {
+  component: Signup,
+  title: 'Components/Signup',
+};
+
+export const Default = () => <Signup />;
---
+++ src/App/components/Signup/Signup.tsx
@@ -0,0 +1,9 @@
+import styles from './Signup.module.css';
+
+export default function Signup(): JSX.Element {
+  return (
+    <div>
+      <span className={styles.signup}>Lets trample!</span>
+    </div>
+  );
+}
