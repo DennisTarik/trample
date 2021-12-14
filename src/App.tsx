d95737f9735d8007984f95f43b0cedc0bc493ff3
DennisTarik <dm499555@gmail.com>
2021-12-14 14:55:24 +0100

    :fire: remove useless code

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,6 @@
-import { useEffect, useState } from 'react';
 import styles from './App.module.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    async function doFetch() {
-      const response = await fetch('/api/hello');
-      const result = await response.json();
-      setMessage(result.message);
-    }
-    doFetch();
-
-    // fetch('/api/hello')
-    //   .then((response) => response.json())
-    //   .then((result) => setMessage(result.message));
-  }, []);
-
   return <div className={styles.app}>test</div>;
 }
 export default App;
